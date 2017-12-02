15b23471744bb474a4acd3907c662d8bc1e132e7
Mayank <bajaj3743@gmail.com>
2017-12-03 02:47:05 +0530

    a

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,28 @@
 class HomeController < ApplicationController
+
+	 protect_from_forgery with: :exception
+
+
+	id=0
   def index
-  	@p=Patient.first
+  	@treatments = Treatment.where(isDiagnosed: false)
+  	@patients=Patient.all
+
   end
+
+
+  def profile
+
+  	patient= Patient.find_by_id(params[:patient_id])
+  	id=params[:patient_id]
+  	    # return redirect_to '/profilepage'
+
+  	end
+
+
+  	def profilepage
+  		@patient=Patient.find_by_id(id)
+  	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,20 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
     post 'patients/new'
     post 'treatments/new'
 
   # Example of regular route:
     get 'home/index'
+    post 'home/profile'
+
+
+    get 'home/profilepage'
+
+
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
